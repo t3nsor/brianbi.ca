5740cf7b5e9dbe7cbfcc77879e728e90a9aaf71a
Brian Bi <brian@wcipeg.com>
2018-09-17 23:54:08 +0000

    update for change of location and employer

--- pages/about.php
+++ pages/about.php
@@ -1,16 +1,10 @@
 <section style="margin: 1em">
 <p>Hi, my name is Brian and I write code. (I'm also Canadian, as you can
-probably tell from the domain name.) Currently I live in Sunnyvale, California
-and I work at Google HQ in Mountain View. Here's one of the few well-lit photos
-that shows my entire face:</p>
+probably tell from the domain name.) Currently I live in Manhattan, NY and I
+work at Citadel Securities on the Low Latency Engineering team.</p>
 
-<figure style="display: inline-block; width: 400px">
-<img src="/images/varsity_feynman.jpg" width="400"/>
-<figcaption style="font-size: 75%; margin: 0.5em">Note that I don't actually
-have permission to use this image, so it may be removed at any time...
-hopefully by that time I'll have another non-crappy photo ready to replace
-it.</figcaption>
-</figure>
+<p>I deleted the photo of me that used to be here, because I don't really look
+like that anymore. Maybe eventually I'll post a new one.</p>
 
 <p>I can be reached by email at
 <a href="mailto:bbi5291@gmail.com">bbi5291@gmail.com</a>.</p>
@@ -19,9 +13,7 @@ it.</figcaption>
 <ul>
 <li><a href="https://www.facebook.com/algosthenes">Facebook</a></li>
 <li><a href="https://twitter.com/@t3nsor">Twitter</a></li>
-<li><a href="https://plus.google.com/112298788988861486912">Google+</a>
-(Note: Just because I work at Google doesn't mean I actually <em>use</em>
-Google+.)</li>
+<li><a href="https://plus.google.com/112298788988861486912">Google+</a></li>
 <li><a href="https://www.linkedin.com/profile/view?id=127253742">LinkedIn</a>
 </li>
 <li><a href="https://www.quora.com/Brian-Bi">Quora</a></li>
