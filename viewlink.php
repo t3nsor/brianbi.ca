2757cc9a355b2102c0e7cd18930eac38f44d3b28
Brian Bi <brian@wcipeg.com>
2020-04-24 19:29:21 +0000

    Add link echo service

---
+++ viewlink.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>Link echo service (suppresses thumbnails)</title>
+</head>
+<body>
+Click to go to <a href="<?=htmlspecialchars($_GET['url'])?>"><?=
+htmlspecialchars($_GET['url'])?></a>
+</body>
+</html>
