c0ad9c6184189474f786e245b7a28b5088b0c73b
Brian Bi <brian@wcipeg.com>
2020-04-27 18:22:28 +0000

    Add form to viewlink.php for easy generation

--- viewlink.php
+++ viewlink.php
@@ -5,7 +5,11 @@
 <title>Link echo service (suppresses thumbnails)</title>
 </head>
 <body>
-Click to go to <a href="<?=htmlspecialchars($_GET['url'])?>"><?=
-htmlspecialchars($_GET['url'])?></a>
+<p>Click to go to <a href="<?=htmlspecialchars($_GET['url'])?>"><?=
+htmlspecialchars($_GET['url'])?></a></p>
+<form action="<?=htmlspecialchars($_SERVER['SCRIPT_NAME'])?>" method="get">
+<p>Enter a url here to generate a link like this one:
+<input type="text" name="url"><input type="submit" value="Generate!"></p>
+</form>
 </body>
 </html>
